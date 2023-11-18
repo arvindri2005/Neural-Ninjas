4994204b9306f5f5e96cbf65cf87bf4d18e4925b
Arvind <arvindri2005@gmail.com>
2023-11-18 23:57:32 +0530

    Change response of user when alreday exist

--- backend/controllers/userController.mjs
+++ backend/controllers/userController.mjs
@@ -13,9 +13,14 @@ const registerUser = asyncHandler(async (req, res) => {
   console.log(req.body);
   const userExists = await User.findOne({email});
 
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
+  if(userExists && (await userExists.matchPassword(password))){
+    res.json({
+      _id:user._id,
+      name:user.name,
+      email:user.email,
+      pic:user.pic,
+      token:user.token
+    })
   }
 
   const user = await User.create({
@@ -45,7 +50,6 @@ const authUser = asyncHandler(async(req,res) => {
   const{email,password} = req.body;
   
   const user = await User.findOne({email});
-  console.log(user);
 
   if(user && (await user.matchPassword(password))){
     res.json({
